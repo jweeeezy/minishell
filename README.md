a9b6477f2367268613a3a8ac528cf560e0920b46
kvebers <kvebers@student.42heilbronn.de>
2023-02-20 16:49:24 +0100

    Made changes in readibility

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # minishell
+
+#This is the minishell project in school 42 Heilbronn!
--- src/test.c
+++ src/test.c
@@ -6,17 +6,17 @@
 /*   By: kvebers <kvebers@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 14:13:47 by kvebers           #+#    #+#             */
-/*   Updated: 2023/02/20 15:45:43 by kvebers          ###   ########.fr       */
+/*   Updated: 2023/02/20 16:48:40 by kvebers          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../minishell.h"	// readline
+#include "../minishell.h"
 
 int	argument_protection(t_data *data, int argc, char **argv, char **envp)
 {
 	if (argc > 1)
 		return (1);
-	data->envp = envp;
+	data->envp = envp;	
 	data->argv = argv;
 	return (0);
 }
@@ -30,7 +30,18 @@ int	init_data(t_data *data)
 	return (0);
 }
 
-
+int	history(t_data *data)
+{
+	data->line = readline(data->promt);
+	if (data->line == NULL)
+		return (1);
+	else if (data->line)
+	{
+		add_history(data->line);
+	}
+	free(data->line);
+	return (0);
+}
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -39,19 +50,13 @@ int	main(int argc, char **argv, char **envp)
 	data = malloc(sizeof(t_data));
 	using_history();
 	if (argument_protection(data, argc, argv, envp) == 1)
-		return (0);
+		return (1);
 	if (init_data(data) == 1)
-		return (0);
+		return (1);
 	while (1)
 	{
-		data->line = readline(data->promt);
-		if (!data->line)
+		if (history(data) == 1)
 			break ;
-		else if (data->line)
-		{
-			add_history(data->line);
-		}
-		free(data->line);
 	}
 	free_memory(data);
 	return (0);
