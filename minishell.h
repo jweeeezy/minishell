cee7bb809f120bde682ebc895400e6261bde3ade
kvebers <kvebers@student.42heilbronn.de>
2023-02-23 16:31:23 +0100

    Tokenizer List and Tokenizer Free

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: kvebers <kvebers@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 14:16:43 by kvebers           #+#    #+#             */
-/*   Updated: 2023/02/21 18:11:37 by kvebers          ###   ########.fr       */
+/*   Updated: 2023/02/23 16:01:47 by kvebers          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 
 typedef struct Line
 {
-	char		*data;
+	char		*str;
 	struct Line	*next;
 }	t_line;
 
@@ -37,6 +37,7 @@ typedef struct data
 
 //free.c
 void	free_after_break(t_data *data);
+char	**free_tokens(char **tokens);
 void	free_loop(t_data *data);
 int		parsing(t_data *data);
 int		argument_protection(t_data *data, int argc, char **argv, char **envp);
