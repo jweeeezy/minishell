b8a7d2d4c2793b25267b2f4feaecd2ea4e068122
jweeeezy <jwillert@42heilbronn.student.de>
2023-02-25 14:28:19 +0100

    Review: Added new suggested code for env_to_list

--- src/env_to_list.c
+++ src/env_to_list.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env_to_list.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kvebers <kvebers@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jwillert <jwillert@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 17:47:50 by kvebers           #+#    #+#             */
-/*   Updated: 2023/02/25 13:06:50 by kvebers          ###   ########.fr       */
+/*   Updated: 2023/02/25 14:27:29 by jwillert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,4 +50,33 @@ int	put_to_linked_list_expander(t_data *data, char **envp)
 		i++;
 	}
 	return (0);
+}
+
+//	@note suggested code
+
+int	put_to_linked_list_expander_new(t_data *data, char **envp)
+{
+	t_expander	*current_node;
+	size_t		index;
+
+	current_node = data->expander;
+	index = 0;
+	while (envp[index] != NULL)
+	{
+		if (data->expander == NULL)
+			data->expander = create_new_expander_node(ft_strdup(envp[index]));
+		else (t_expander_add_back(data->expander, create_new_expander_node(ft_strdup(envp[index]))) == -1)
+			return (-1);
+		index++;
+	}
+	return (0);
+}
+
+int	t_expander_add_back(t_expander *lst_to_expand, t_expander *node_to_add)
+{
+	if (lst_to_expand == NULL || node_to_add == NULL)
+		return (-1);
+	while (lst_to_expand->next != NULL)
+		lst_to_expand = lst_to_expand->next;
+	lst_to_expand->next = node_to_add;
 }
