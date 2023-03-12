1737adca699b421c5a8f583acd332b2bcfa9ef75
jweeeezy <jwillert@42heilbronn.student.de>
2023-03-12 10:30:01 +0100

    started writing pipe code

--- 06_executor/src/executor_pipe.c
+++ 06_executor/src/executor_pipe.c
@@ -6,20 +6,46 @@
 /*   By: jwillert@student.42heilbronn.de            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 18:35:10 by jwillert          #+#    #+#             */
-/*   Updated: 2023/03/10 18:55:45 by jwillert         ###   ########          */
+/*   Updated: 2023/03/11 21:08:35 by jwillert         ###   ########          */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	executor_pipe(t_data *data, int cnt)
-{
-	t_vector_str	vector_cmd;
-	t_execute		*offset;
-	int				index;
+#include <unistd.h>
 
-	index = 0;
-	offset = data->execute[cnt];
-	while (data->execute[index] != offset)
+int	executor_pipe(t_data *data)
+{
+	int fd[2];
+
+	if (pipe(fd) == -1)
 	{
+		perror("pipe");
+		return (ERROR);
+	}
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		return (ERROR);
+	}
+
+	if (pid == 0)
+	{
+		close(fd[1]);
+
+		// do something
+
+		close(fd[0]);
+	}
+
+	else
+	{
+		close(fd[0]);
+
+		// do something
+
+		close(fd[1]);
 		
 	}
-	// prepare pipes
+	return (0);
+}
