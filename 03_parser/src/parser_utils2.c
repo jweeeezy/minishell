9dc7fa119b9a6bd20fe4d59776e76516e7947228
kvebers <kvebers@student.42heilbronn.de>
2023-03-12 21:43:52 +0100

    Parser offset

--- 03_parser/src/parser_utils2.c
+++ 03_parser/src/parser_utils2.c
@@ -6,7 +6,7 @@
 /*   By: kvebers <kvebers@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 15:30:31 by kvebers           #+#    #+#             */
-/*   Updated: 2023/03/12 20:23:49 by kvebers          ###   ########.fr       */
+/*   Updated: 2023/03/12 21:26:01 by kvebers          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,30 @@
 #include <stdio.h>
 #include "libft.h"
 
+// @todo Write Strnstrn2
 char	*search_needle(t_data *data, char *needle)
 {
-	char		*result_str;
 	t_expander	*current_node;
+	char		*temp_needle;
 
 	current_node = data->expander;
-	result_str = NULL;
-	while (current_node != NULL && result_str == NULL)
+	temp_needle = ft_strjoin(needle, "=");
+	if (temp_needle == NULL)
+		return (NULL);
+	while (current_node != NULL)
 	{
-		if (ft_strnstr(current_node->str, needle,
+		if (ft_strnstr(current_node->str, temp_needle,
 				ft_strlen(current_node->str)) != NULL)
-			return (current_node->str);
+			return (free(temp_needle), current_node->str);
 		current_node = current_node->next;
 	}
-	return (NULL);
+	return (free(temp_needle), NULL);
 }
 
 int	dolla_handler(t_data *data, int cnt, int cnt1)
 {
 	char	*dolla;
+	char	*dolla_offset;
 
 	if (cnt + 1 < data->tokens && data->execute[cnt + 1].order_numb != WHITE
 		&& is_pipe(data->execute[cnt + 1].order_numb) != ADD)
@@ -41,8 +45,11 @@ int	dolla_handler(t_data *data, int cnt, int cnt1)
 		dolla = search_needle(data, data->execute[cnt + 1].order_str);
 		if (dolla != NULL)
 		{
+			dolla_offset = dolla
+				+ ft_strlen(data->execute[cnt + 1].order_str) + 1;
 			data->combine[cnt1].combined_str
-				= ft_strjoin2(data->combine[cnt1].combined_str, dolla, 0, 0);
+				= ft_strjoin2(data->combine[cnt1].combined_str,
+					dolla_offset, 0, 0);
 			return (EXECUTED);
 		}
 		return (EXECUTED);
