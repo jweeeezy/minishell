4ab1dd6b8d9fb51bbeb6fe855e5d74917349fe62
jweeeezy <jwillert@42heilbronn.student.de>
2023-03-14 11:13:44 +0100

    WIP: cleaning up exec code

---
+++ 06_executor/src/executor_setup.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   executor_setup.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jwillert@student.42heilbronn.de            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/14 09:57:37 by jwillert          #+#    #+#             */
+/*   Updated: 2023/03/14 11:10:55 by jwillert         ###   ########          */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+#include "libme.h"
+
+int	try_access(t_execute *execute, char *path, char *command)
+{
+	execute->full_path = ft_str_join_delimiter(path, "/", command);
+	if (execute->full_path == NULL)
+	{
+		return (ERROR);
+	}
+	if (access(execute->full_path, X_OK) != -1)
+	{
+		free(execute_full_path);
+		execute->full_path = NULL;
+		return (ERROR);
+	}
+	free(execute_full_path);
+	execute->full_path = NULL;
+	return (EXECUTED);
+}
+
+int	check_valid_command(t_execute *offset, char **envp)
+{	
+	int		return_value;
+	int		index;
+	char	**paths;
+
+	return_value = 0;
+	index = 0;
+	paths = get_path_array(envp);
+	if (paths == NULL)
+	{
+		return (ERROR);
+	}
+	while (paths[index] != NULL)
+	{
+		return_value = try_access(offset, paths[index], offset->order_str);
+		if (return_value != 0)
+		{
+			free_char_array(paths);
+			return (return_value);
+		}
+		index += 1;
+	}
+	free_char_array(paths);
+	return (return_value);
+}
+
+int	convert_command_to_vector(t_data *data, t_execute *offset)
+{
+	t_vector_str	*vector_args;
+
+	vector_args = data->vector_args;
+	while (offset->order_str != NULL && offset->order_numb != 5)
+	{
+		vector_args = ft_vector_str_join(vector_args, offset->order_str, 0);
+		if (vector_args == NULL)
+		{
+			return (ERROR);
+		}
+		offset += 1;
+	}
+	data->vector_args = vector_args;
+	debug_print_t_vector_str(data->vector_args);
+	return (0);
+}
+
+char	**convert_vector_to_array(t_data *data)
+{
+	char	**array_command;
+
+	array_command = NULL;
+	if (data->vector_args != NULL)
+	{
+		array_command = ft_split(data->vector_args->str, ' ');
+		ft_vector_str_free(data->vector_args);
+		data->vector_args = NULL;
+	}
+	return (array_command);
+}
---
+++ 06_executor/src/executor_utils.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   executor_utils.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jwillert@student.42heilbronn.de            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/14 09:20:28 by jwillert          #+#    #+#             */
+/*   Updated: 2023/03/14 10:08:45 by jwillert         ###   ########          */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"	// needed for t_data, t_execute
+
+int	count_pipes(t_execute *execute)
+{
+	int	index_str;
+	int	index_array;
+	int	counter;
+
+	index_str = 0;
+	index_array = 0;
+	counter = 0;
+	while (execute[index_array].order_str != NULL)
+	{
+		while (execute[index_array].order_str[index_str] != '\0')
+		{
+			if (execute[index_array].order_str[index_str] == '|')
+			{
+				counter += 1;
+			}
+			index_str += 1;
+		}
+		index_str = 0;
+		index_array += 1;
+	}
+	return (counter);
+}
+
+char	**get_path_array(char **envp)
+{
+	char	**path_array;
+
+	path_array = NULL;
+	while (envp != NULL && *envp != NULL)
+	{
+		if (ft_str_check_needle(*envp, "PATH=", ft_strlen(*envp)) == 1)
+		{
+			path_array = ft_split(*envp + 5, ':');
+			break ;
+		}
+		envp += 1;
+	}
+	return (path_array);
+}
+
+t_execute	*get_pipe(t_execute *offset)
+{
+	int	index;
+
+	index = 0;
+	while (offset[index].order_str != NULL
+		&& offset[index].order_numb != PIPE
+		&& offset[index].order_numb != PIPE_LAST)
+	{
+		index += 1;
+	}
+	return (&offset[index]);
+}
+
+t_execute	*get_string_after_pipe(t_execute *offset, t_execute *next_pipe)
+{
+	int	index;
+
+	index = 0;
+	while (&offset[index] != next_pipe
+		&& offset[index].order_str != NULL)
+	{
+		index += 1;
+	}
+	while (offset[index].order_numb != STRING
+		&& offset[index].order_str != NULL)
+	{
+		index += 1;
+	}
+	return (&offset[index]);
+}
+
+t_execute	*get_string_after_whitespaces(t_execute *execute)
+{
+	int	index;
+
+	index = 0;
+	if (execute == NULL)
+	{
+		return (NULL);
+	}
+	while (execute[index].order_numb == 1 || execute[index].order_numb == 2)
+	{
+		index += 1;
+	}
+	return (&execute[index]);
+}
