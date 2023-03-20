720752f18ea82bfddd1d7824165b789663d106f3
kvebers <kvebers@student.42heilbronn.de>
2023-03-20 17:21:18 +0100

    Some more error managment

--- 03_parser/src/parser_error1.c
+++ 03_parser/src/parser_error1.c
@@ -6,7 +6,7 @@
 /*   By: kvebers <kvebers@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 11:11:26 by kvebers           #+#    #+#             */
-/*   Updated: 2023/03/20 16:12:38 by kvebers          ###   ########.fr       */
+/*   Updated: 2023/03/20 17:20:25 by kvebers          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,13 +47,39 @@ int	error_conseq_seperators(t_data *data, int cnt, int q_o, int last_command)
 			q_o = data->execute[cnt].order_numb;
 		else if (q_o == data->execute[cnt].order_numb)
 			q_o = 0;
-		if (cnt + 1 < data->tokens && data->execute[cnt].order_numb
-			&& data->execute[cnt + 1].order_numb && q_o == 0)
+		if (cnt + 1 < data->tokens
+			&& is_micro_pipe(data->execute[cnt].order_numb)
+			&& is_micro_pipe(data->execute[cnt + 1].order_numb) && q_o == 0)
 			return (ERROR);
-		if (last_command != WHITE)
-			last_command = data->execute[cnt + 1].order_numb;
+		if (data->execute[cnt].order_numb != WHITE)
+			last_command = data->execute[cnt].order_numb;
 		cnt++;
 	}
+	if (is_pipe(last_command) == ADD)
+		return (ERROR);
+	return (EXECUTED);
+}
+
+int	error_3_sep(t_data *data, int cnt, int q_o, int last_command)
+{
+	while (cnt < data->tokens)
+	{
+		if (q_o == 0 && (data->execute[cnt].order_numb == QUOTATION_MARK
+				|| data->execute[cnt].order_numb == APOSTROPHE))
+			q_o = data->execute[cnt].order_numb;
+		else if (q_o == data->execute[cnt].order_numb)
+			q_o = 0;
+		if (cnt + 2 < data->tokens
+			&& is_pipe(data->execute[cnt].order_numb)
+			&& is_pipe(data->execute[cnt + 1].order_numb) && q_o == 0
+			&& is_pipe(data->execute[cnt + 2].order_numb) && q_o == 0)
+			return (ERROR);
+		if (data->execute[cnt].order_numb != WHITE)
+			last_command = data->execute[cnt].order_numb;
+		cnt++;
+	}
+	if (is_pipe(last_command) == ADD)
+		return (ERROR);
 	return (EXECUTED);
 }
 
@@ -61,7 +87,17 @@ int	pipe_error_handeler(t_data *data)
 {
 	if (error_consequatives_pipes(data, 0, 0, 0) == ERROR)
 	{
-		printf("ERROR: Syntax error near |\n");
+		printf("ERROR: Do not activate the forbiden magic with |\n");
+		return (ERROR);
+	}
+	else if (error_conseq_seperators(data, 0, 0, 0) == ERROR)
+	{
+		printf("ERROR: You have some sketchy command there with <>|\n");
+		return (ERROR);
+	}
+	else if (error_3_sep(data, 0, 0, 0) == ERROR)
+	{
+		printf("ERROR: Bozo, BOZO! You have too many of these bad boys <>|\n");
 		return (ERROR);
 	}
 	return (EXECUTED);
