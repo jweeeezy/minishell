64131f6444d4bb86a037dbd1eea54fc1bfa5f1d6
jweeeezy <jwillert@42heilbronn.student.de>
2023-03-20 11:14:12 +0100

    WIP: cleaned up code, need to test still

--- 06_executor/src/executor_main.c
+++ 06_executor/src/executor_main.c
@@ -6,7 +6,7 @@
 /*   By: jwillert <jwillert@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 09:02:06 by jwillert          #+#    #+#             */
-/*   Updated: 2023/03/14 11:12:57 by jwillert         ###   ########          */
+/*   Updated: 2023/03/20 11:13:33 by jwillert         ###   ########          */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,22 +23,20 @@ static void	child_routine(t_data *data, t_execute *offset,
 		char **array_command, int *fd_pipe)
 {
 	debug_print_char_array(array_command);
-
 	if (fd_pipe != NULL)
 	{
 		close(fd_pipe[1]);
-		dup2(fd_pipe[0], STDOUT_FILENO);
 	}
 	if (execve(offset->full_path, array_command, data->envp) == -1)
 	{
-		//@note needed?
+		//@note freeing in child needed?
 		free_char_array(array_command);
 		perror("execve");
 		exit (ERROR);
 	}
 }
 
-int	execute_in_child(t_data *data, t_execute *offset,
+static int	execute_in_child(t_data *data, t_execute *offset,
 		char **array_command, int *fd_pipe)
 {
 	int	id;
@@ -56,8 +54,7 @@ int	execute_in_child(t_data *data, t_execute *offset,
 	{
 		if (fd_pipe != NULL)
 		{
-			close(fd_pipe[1]);
-			dup2(fd_pipe[0], STDIN_FILENO);
+			close(fd_pipe[0]);
 		}
 		if (wait(NULL) == -1)
 		{
@@ -65,177 +62,82 @@ int	execute_in_child(t_data *data, t_execute *offset,
 		}
 	}
 	// @note freeing setting to NULL
+	close(fd_pipe[1]);
 	free_char_array(array_command);
 	return (EXECUTED);
 }
 
-static int	executor_pipe(t_data *data, t_execute *offset,
-	   			t_execute *next_pipe, int *fd_pipe)
+
+static int	executor_routine(t_data *data, t_execute *offset,
+		t_execute *next_pipe, int *fd_pipe)
 {
-	pid = fork();
-	if (pid == -1)
+	int			return_value;
+	char		**array_command;
+
+	return_value = 0;
+	return_value = check_valid_command(offset, data->envp);
+	if (return_value == 1)
 	{
-		return (ERROR);
-	}
-	else if (pid == 0)
-	{
-		close(fd_pipe[0]);
-		dup2(fd_pipe[1], STDOUT_FILENO);
-		if (execve(offset->full_path, arg_array, data->envp) == -1)
-		{
-			perror("execve");
-		}
-		exit(ERROR);
-	}
-	else
-	{
-		pid = fork();
-		if (pid == -1)
+		if (convert_command_to_vector(data, offset) == ERROR)
 		{
 			return (ERROR);
 		}
-		else if (pid == 0)
+		array_command = convert_vector_to_array(data);
+		if (array_command == NULL)
 		{
-			close(fd_pipe[1]);
-			dup2(fd_pipe[0], STDIN_FILENO);
-			ft_vector_str_free(data->vector_args);
-			data->vector_args = NULL;
-			offset = executor_t_execute_advance_offset(offset, next_pipe);
-			if (executor_check_valid_command(data, offset) != 1)
-			{
-				exit(ERROR);
-			}
-			executor_get_command_arguments(data, offset);
-			free_char_array(arg_array);
-			if (data->vector_args != NULL)
-				arg_array = ft_split(data->vector_args->str, ' ');
-			else
-			{
-				arg_array = NULL;
-			}
-			ft_vector_str_free(data->vector_args);
-			data->vector_args = NULL;
-			if (execve(offset->full_path, arg_array, data->envp) == -1)
-			{
-				perror("execve");
-			}
-			exit(ERROR);
+			return (ERROR);
 		}
-		else
+		if (execute_in_child(data, offset, array_command, fd_pipe) == ERROR)
 		{
-			close(fd_pipe[0]);
-			wait(NULL);
+			return (ERROR);
 		}
-		close(fd_pipe[1]);
-		wait(NULL);
 	}
-	ft_vector_str_free(data->vector_args);
-	data->vector_args = NULL;
-	return (EXECUTED);
+	return (return_value);
 }
 
 int	executor_main(t_data *data)
 {
-	t_execute	*offset;
-	t_execute	*next_pipe;
-	int			return_value;
 	int			counter_pipes;
 	int			fd_pipe[2];
 	int			fd_stdin;
 	int			fd_stdout;
+	t_execute	*next_pipe;
+	t_execute	*offset;
 
-	fd_stdin = dup(STDIN_FILENO);
-	fd_stdout = dup(STDOUT_FILENO);
-
-	return_value = 0;
-	counter_pipes = 0;
-
-	pipe(fd_pipe);
-	// @note mb parsing will loop through white spaces already?
+	if (pipe(fd_pipe) == ERROR)
+	{
+		return (ERROR);
+	}
 	offset = executor_loop_whitespaces(data->execute);
-	// @note mb while loop so it goes until no pipe is left, but it should also
-	// advance the offset to the next order_numb == STRING
+	counter_pipes = count_pipes(offset);
 	if (offset == NULL)
 	{
 		return (ERROR);
 	}
-	counter_pipes = count_pipes(offset);
-	debug_print_int("Counter pipes: ", counter_pipes);
+	
+	fd_stdin = dup(STDIN_FILENO);
+	fd_stdout = dup(STDOUT_FILENO);
 
-	while (counter_pipes > 1)
+	dup2(fd_pipe[0], STDIN_FILENO);
+	dup2(fd_pipe[1], STDOUT_FILENO);
+
+	while (counter_pipes > 0)
 	{
-		
-		printf("Reached\n");
-		next_pipe = executor_t_execute_get_pipe(offset);
+		next_pipe = get_pipe(offset);
 		if (next_pipe->order_str != NULL)
 		{
-			return_value = executor_check_valid_command(data, offset);
-			if (return_value == 1)
-			{
-				if (executor_get_command_arguments(data, offset) == ERROR)
-				{
-					return (ERROR);
-				}
-				else if (next_pipe->order_numb == PIPE)
-				{
-					dup2(fd_pipe[0], STDIN_FILENO);
-					dup2(fd_pipe[1], STDOUT_FILENO);
-					if (executor_pipe(data, offset, next_pipe, fd_pipe) == ERROR)
-					{
-						return (ERROR);
-					}
-				}
-			}
-		}
-		counter_pipes -= 1;
-		offset = next_pipe + 1;
-		offset = executor_loop_whitespaces(offset);
-	}
-	if (counter_pipes == 1)
-	{
-		printf("Reached 2\n");
-		next_pipe = executor_t_execute_get_pipe(offset);
-		if (next_pipe->order_str != NULL)
-		{
-			return_value = executor_check_valid_command(data, offset);
-			if (return_value == 1)
-			{
-				if (executor_get_command_arguments(data, offset) == ERROR)
-				{
-					return (ERROR);
-				}
-				if (next_pipe->order_numb == PIPE)
-				{
-					if (executor_pipe(data, offset,
-								next_pipe, fd_pipe) == ERROR)
-					{
-						return (ERROR);
-					}
-				}
-			}
-		}
-	}
-	else if (offset->order_numb == STRING)
-	{
-		// @todo extract code as execute extern command
-		printf("Reached 3\n");
-		return_value = executor_check_valid_command(data, offset);
-		if (return_value == 1)
-		{
-			if (executor_get_command_arguments(data, offset) == ERROR)
-			{
-				return (ERROR);
-			}
-			if (executor_try_execve(data, offset) == ERROR)
+			if (executor_routine(data, offset, next_pipe, fd_pipe) == ERROR)
 			{
 				return (ERROR);
 			}
 		}
-		//	@note can prob save this part and just return return_value
-		else if (return_value == ERROR)
-		{
-			return (ERROR);
-		}
 	}
-	return (0);
+
+	dup2(STDIN_FILENO, ft_stdin);
+	dup2(STDOUT_FILENO, ft_stdout);
+	if (executor_routine(data, offset, next_pipe, fd_pipe) == ERROR)
+	{
+		return (ERROR);
+	}
 }
+
