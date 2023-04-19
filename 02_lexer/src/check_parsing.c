62aabd539e796e40b05f3d81f1cab03c181b0871
kvebers <kvebers@student.42heilbronn.de>
2023-04-19 15:05:43 +0200

    10 more cases fixed

--- 02_lexer/src/check_parsing.c
+++ 02_lexer/src/check_parsing.c
@@ -6,14 +6,13 @@
 /*   By: kvebers <kvebers@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 11:01:45 by kvebers           #+#    #+#             */
-/*   Updated: 2023/04/19 13:58:26 by kvebers          ###   ########.fr       */
+/*   Updated: 2023/04/19 15:04:38 by kvebers          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "libft.h"
 #include <stdio.h>
-
 
 int	quoter(t_data *data, int cnt, int q)
 {
@@ -30,12 +29,36 @@ int	quoter(t_data *data, int cnt, int q)
 	return (EXECUTED);
 }
 
+int	piper(t_data *data)
+{
+	size_t	cnt;
+
+	cnt = 0;
+	while (data->line[cnt] != '\0')
+	{
+		if (data->line[0] == '|')
+			return (ERROR);
+		if (cnt == ft_strlen(data->line) - 1)
+		{
+			if (data->line[cnt] == '|')
+				return (ERROR);
+		}
+		cnt++;
+	}
+	return (EXECUTED);
+}
+
 int	check_parsing(t_data *data)
 {
 	char	*temp;
 
 	temp = NULL;
-	if (quoter(data, 0, 0) == ERROR || data->line == NULL)
+	if (quoter(data, 0, 0) == ERROR)
+	{
+		ft_putstr_fd("ERROR\n", 2);
+		return (ERROR);
+	}
+	if (piper(data) == ERROR)
 	{
 		ft_putstr_fd("ERROR\n", 2);
 		return (ERROR);
